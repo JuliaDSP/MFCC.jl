d68cf4ff6dea42401c506ad61b8066308a9a35db
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2014-10-09 18:23:42 +0200

    Module name now MFCC

--- src/MFCC.jl
+++ src/MFCC.jl
@@ -1,9 +1,9 @@
-## MFCCs.jl
+## MFCC.jl
 ## (c) 2013-2014 David A. van Leeuwen, (c) 2005--2012 Dan Ellis
 
 ## Recoded from / inspired by melfcc from Dan Ellis's rastamat package. 
 
-module MFCCs
+module MFCC
 
 export powspec, audspec, postaud, lifter
 export mfcc, deltas, warp, sdc, znorm, znorm!
