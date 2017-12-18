2cb241935e2c94e55011504da7bda9de31a68756
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2017-12-18 22:46:55 +0100

    Some more forgoten dots in function.(), whitespace

--- src/rasta.jl
+++ src/rasta.jl
@@ -41,7 +41,7 @@ function audspec{T<:AbstractFloat}(x::Array{T}, sr::Real=16000.0; nfilts=iceil(h
     else
         error("Unknown filterbank type ", fbtype)
     end
-    wts = wts[:,1:nfreqs]
+    wts = wts[:, 1:nfreqs]
     if sumpower
         return wts * x
     else
@@ -50,18 +50,18 @@ function audspec{T<:AbstractFloat}(x::Array{T}, sr::Real=16000.0; nfilts=iceil(h
 end
 
 function fft2barkmx(nfft::Int, nfilts::Int; sr=8000.0, width=1.0, minfreq=0., maxfreq=sr/2)
-    hnfft = nfft>>1
+    hnfft = nfft >> 1
     minbark = hz2bark(minfreq)
     nyqbark = hz2bark(maxfreq) - minbark
     wts=zeros(nfilts, nfft)
     stepbark = nyqbark/(nfilts-1)
     binbarks=hz2bark([0:hnfft]*sr/nfft)
-    for i=1:nfilts
+    for i in 1:nfilts
         midbark = minbark + (i-1)*stepbark
         lof = (binbarks - midbark)/width - 0.5
         hif = (binbarks - midbark)/width + 0.5
         logwts = min(0, hif, -2.5lof)
-        wts[i,1:1+hnfft] = 10.0.^logwts
+        wts[i, 1:1+hnfft] = 10.0.^logwts
     end
     return wts
 end
@@ -97,13 +97,13 @@ function fft2melmx(nfft::Int, nfilts::Int; sr=8000.0, width=1.0, minfreq=0.0, ma
         wts = broadcast(*, 2 ./ ((binfreqs[3:nfilts+2]) - binfreqs[1:nfilts]), wts)
     end
     # Make sure 2nd half of DFT is zero
-    wts[:,(nfft>>1)+1:nfft] = 0.
+    wts[:, (nfft>>1)+1:nfft] = 0.
     return wts
 end
 
 function hz2mel{T<:AbstractFloat}(f::Vector{T}, htk=false)
     if htk
-        return 2595 * log10(1+f/700)
+        return 2595 * log10.(1 + f/700)
     else
         f0 = 0.0
         fsp = 200/3
@@ -121,7 +121,7 @@ hz2mel(f::AbstractFloat, htk=false)  = hz2mel([f], htk)[1]
 
 function mel2hz{T<:AbstractFloat}(z::Vector{T}, htk=false)
     if htk
-        f = 700*(10.^(z/2595)-1)
+        f = 700 * (10.^(z/2595) - 1)
     else
         f0 = 0.0
         fsp = 200/3
@@ -130,20 +130,20 @@ function mel2hz{T<:AbstractFloat}(z::Vector{T}, htk=false)
         logstep = exp(log(6.4)/27)
         linpts = z .< brkpt
         f = similar(z)
-        f[linpts] = f0 .+ fsp*z[linpts]
-        f[.!linpts] = brkfrq .* exp.(log.(logstep)*(z[.!linpts] .- brkpt))
+        f[linpts] = f0 .+ fsp * z[linpts]
+        f[.!linpts] = brkfrq .* exp.(log.(logstep) * (z[.!linpts] .- brkpt))
     end
     return f
 end
 
 function postaud{T<:AbstractFloat}(x::Matrix{T}, fmax::Real, fbtype=:bark, broaden=false)
     (nbands,nframes) = size(x)
-    nfpts = nbands+2broaden
-    if fbtype==:bark
+    nfpts = nbands + 2broaden
+    if fbtype == :bark
         bandcfhz = bark2hz(linspace(0, hz2bark(fmax), nfpts))
-    elseif fbtype==:mel
+    elseif fbtype == :mel
         bandcfhz = mel2hz(linspace(0, hz2mel(fmax), nfpts))
-    elseif fbtype==:htkmel || fbtype==:fcmel
+    elseif fbtype == :htkmel || fbtype == :fcmel
         bandcfhz = mel2hz(linspace(0, hz2mel(fmax,1), nfpts),1);
     else
         error("Unknown filterbank type")
@@ -169,7 +169,7 @@ end
 
 function dolpc{T<:AbstractFloat}(x::Array{T}, modelorder::Int=8)
     nbands, nframes = size(x)
-    r = real(ifft(vcat(x, x[collect(nbands-1:-1:2),:]), 1)[1:nbands,:])
+    r = real(ifft(vcat(x, x[collect(nbands-1:-1:2),:]), 1)[1:nbands, :])
     # Find LPC coeffs by durbin
     y, e = levinson(r, modelorder)
     # Normalize each poly by gain
@@ -179,19 +179,19 @@ end
 function lpc2cep{T<:AbstractFloat}(a::Array{T}, ncep::Int=0)
     nlpc, nc = size(a)
     order = nlpc - 1
-    if ncep==0
-        ncep=nlpc
+    if ncep == 0
+        ncep = nlpc
     end
     c = zeros(ncep, nc)
     # Code copied from HSigP.c: LPC2Cepstrum
     # First cep is log(Error) from Durbin
-    c[1,:] = -log(a[1,:])
+    c[1,:] = -log.(a[1, :])
     # Renormalize lpc A coeffs
-    a ./= a[1,:]
-    for n=2:ncep
-        sum=0.0
-        for m=2:n
-            sum += (n-m)*a[m,:] .* c[n-m+1,:]
+    a ./= a[1, :]
+    for n in 2:ncep
+        sum = zero(T)
+        for m in 2:n
+            sum += (n-m) * a[m,:] .* c[n-m+1, :]
         end
         c[n,:] = -a[n,:] - sum/(n-1)
     end
@@ -205,35 +205,35 @@ function spec2cep{T<:AbstractFloat}(spec::Array{T}, ncep::Int=13, dcttype::Int=2
     (nr, nc) = size(spec)
     dctm = zeros(typeof(spec[1]), ncep, nr)
     if 1 < dcttype < 4          # type 2,3
-        for i=1:ncep
-            dctm[i,:] = cos.((i-1) * collect(1:2:2nr-1)π/(2nr)) * sqrt(2/nr)
+        for i in 1:ncep
+            dctm[i, :] = cos.((i-1) * collect(1:2:2nr-1)π/(2nr)) * sqrt(2/nr)
         end
-        if dcttype==2
-            dctm[1,:] /= sqrt(2)
+        if dcttype == 2
+            dctm[1, :] /= sqrt(2)
         end
-    elseif dcttype==4           # type 4
-        for i=1:ncep
-            dctm[i,:] = 2cos((i-1) * collect(1:nr)π/(nr+1))
-            dctm[i,1] += 1
-            dctm[i,nr] += (-1)^(i-1)
+    elseif dcttype == 4           # type 4
+        for i in 1:ncep
+            dctm[i, :] = 2cos.((i-1) * collect(1:nr)π/(nr+1))
+            dctm[i, 1] += 1
+            dctm[i, nr] += (-1)^(i-1)
         end
         dctm /= 2(nr+1)
     else                        # type 1
-        for i=1:ncep
-            dctm[i,:] = cos.((i-1) * collect(0:nr-1)π/(nr-1)) / (nr-1)
+        for i in 1:ncep
+            dctm[i, :] = cos.((i-1) * collect(0:nr-1)π/(nr-1)) / (nr-1)
         end
-        dctm[:,[1,nr]] /= 2
+        dctm[:, [1, nr]] /= 2
     end
     return dctm * log.(spec)
 end
 
 function lifter{T<:AbstractFloat}(x::Array{T}, lift::Real=0.6, invs=false)
     (ncep, nf) = size(x)
-    if lift==0
+    if lift == 0
         return x
     end
-    if lift>0
-        if lift>10
+    if lift > 0
+        if lift > 10
             error("Too high lift number")
         end
         liftw = [1; collect(1:ncep-1).^lift]
@@ -243,7 +243,7 @@ function lifter{T<:AbstractFloat}(x::Array{T}, lift::Real=0.6, invs=false)
             error("Negative lift must be interger")
         end
         lift = -lift            # strictly speaking unnecessary...
-        liftw = vcat(1, (1 + lift/2*sin(collect(1:ncep-1)π/lift)))
+        liftw = vcat(1, (1 + lift/2 * sin.(collect(1:ncep-1)π/lift)))
     end
     if invs
         liftw = 1./liftw
@@ -257,13 +257,13 @@ end
 ## untested
 ## only returns a, v
 function levinson{T<:Real}(acf::Vector{T}, p::Int)
-    if length(acf)<1
+    if length(acf) < 1
         error("empty autocorrelation function")
     end
-    if p<0
+    if p < 0
         error("negative model order")
     end
-    if p<100
+    if p < 100
         ## direct solution [O(p^3), but no loops so slightly faster for small p]
         ## Kay & Marple Eqn (2.39)
         R = toeplitz(acf[1:p], conj(acf[1:p]))
@@ -286,7 +286,7 @@ function levinson{T<:Real}(acf::Vector{T}, p::Int)
         end
         unshift!(a, 1)
     end
-    return (a,v)
+    return (a, v)
 end
 
 function levinson{T<:Real}(acf::Array{T}, p::Int)
@@ -296,16 +296,16 @@ function levinson{T<:Real}(acf::Array{T}, p::Int)
     for i=1:nc
         (a[:,i],v[:,i]) = levinson(acf[:,i], p)
     end
-    return (a,v)
+    return (a, v)
 end
 
-## Freely after octave's implementation, ver 3.2.4, by  jwe && jh
+## Freely after octave's implementation, ver 3.2.4, by jwe && jh
 ## skipped sparse implementation
 function toeplitz{T<:Real}(c::Vector{T}, r::Vector{T}=c)
     nc = length(r)
     nr = length(c)
     res = zeros(typeof(c[1]), nr, nc)
-    if nc==0 || nc==0
+    if nc == 0 || nr == 0
         return res
     end
     if r[1] != c[1]
@@ -317,7 +317,7 @@ function toeplitz{T<:Real}(c::Vector{T}, r::Vector{T}=c)
     end
     ## if issparse(c) && ispsparse(r)
     data = [r[end:-1:2], c]
-    for (i,start)=zip(1:nc, nc:-1:1)
+    for (i, start) in zip(1:nc, nc:-1:1)
         res[:,i] = data[start:start+nr-1]
     end
     return res
